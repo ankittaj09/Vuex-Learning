baae2e7438cdcf92e06a7ae743b6069152b2abed
Ankit Singh <ankittaj035@gmail.com>
2026-03-24 15:22:39 +0530

    Initial Main Code

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <productListOne :product="product"/>
+    <productListTwo :product="product"/>
+  </div>
+</template>
+
+<script>
+import productListOne from './components/productListOne.vue';
+import productListTwo from './components/productListTwo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    productListOne,
+    productListTwo
+  },
+  data(){
+    return{
+      product:[
+        {name:'Banana',price:20},
+        {name:'capsicum',price:40},
+        {name:'carrot',price:60},
+        {name:'Potato',price:100}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+body{
+    font-family: Ubuntu;
+    color: #555;
+}
+</style>
---
+++ src/components/productListOne.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="product-list-one">
+    <h1>Product List One</h1>
+    <ul>
+      <li v-for="item in product" :key="item.name">
+        <span class="name">{{ item.name }}</span>
+        <span class="price"> ₹ {{ item.price }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+
+
+export default {
+  name: 'product1',
+  props:['product'],
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+#product-list-one{
+    background: #FFF8B1;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-one ul{
+    padding: 0;
+}
+#product-list-one li{
+    display: inline-block;
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #E8800C;
+}
+</style>
